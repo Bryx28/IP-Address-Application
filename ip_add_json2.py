7975f0ace20e127acc06e82d9341a8e0669c541d
Bryx28 <qjbfquebral@tip.edu.ph>
2021-11-05 13:25:58 +0000

    Added Class for the User IP and Verification.

--- ip_add_json2.py
+++ ip_add_json2.py
@@ -4,5 +4,6 @@ main_url = 'https://ipapi.co/'
 ip_add = input("Enter your public ip address: ")
 url = main_url + ip_add + "/latitude/"
 lat = get(url).text
-
+status = get(main_url + ip_add + '/').json
+print(status)
 print(lat)
