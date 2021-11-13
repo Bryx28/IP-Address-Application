52941a4c53b5fc10bc0b62b7ba19a1d17aee6204
Bryx28 <qjbfquebral@tip.edu.ph>
2021-11-13 12:38:35 +0000

    Revision of Codes

--- ip_add_json2.py
+++ ip_add_json2.py
@@ -1,9 +1,42 @@
-from requests import get
+import requests
 
-main_url = 'https://ipapi.co/'
-ip_add = input("Enter your public ip address: ")
-url = main_url + ip_add + "/latitude/"
-lat = get(url).text
-status = get(main_url + ip_add + '/').json
-print(status)
-print(lat)
+#Class for Getting the IP Address Information of a Specific User
+class Search_IP_Address:
+
+    #Initialization of API to get Public IP Address of the User
+    def __init__(self, ip_add):
+        self.main_url = f'https://ipapi.co/{ip_add}/json/'
+        self.confirmation = requests.get(f'https://ipapi.co/{ip_add}/latitude/').text
+        self.loc = requests.get(self.main_url)
+        self.ip_info = self.loc.json()
+
+    #Get the Geological Location of a Specific User
+    def get_geological_info(self):
+
+        #Getting the Information of the Location
+        self.ip_loc_country_code = self.ip_info['country_code']
+        self.ip_loc_country = self.ip_info['country_name']
+        self.ip_loc_region = self.ip_info['region']
+        self.ip_loc_latitute = self.ip_info['latitude']
+        self.ip_loc_longitude = self.ip_info['longitude']
+
+        #Display of a Specific User Location based on IP Address
+        print("*------------------ User Location -------------------*")
+        print(f"Country Code: {self.ip_loc_country_code}")
+        print(f"Country Name: {self.ip_loc_country}")
+        print(f"Region: {self.ip_loc_region}")
+        print(f"Latitude: {self.ip_loc_latitute}")
+        print(f"Longitude: {self.ip_loc_longitude}")
+        print("*----------------------------------------------------*")
+    
+    #Getting the ISP and ASN of a Specific User IP Address
+    def get_isp_info(self):
+        #Getting the ISP and ASN
+        self.ip_isp = self.ip_info['org']
+        self.ip_asn = self.ip_info['asn']
+
+        #Display of ISP and ASN of User
+        print("*--------------- User ISP Information ---------------*")
+        print(f"Internet Service Provider: {self.ip_isp}")
+        print(f"Autonomous System Number: {self.ip_asn}")
+        print("*----------------------------------------------------*")
--- ip_address_app.py
+++ ip_address_app.py
@@ -1,27 +1,89 @@
 from ip_add_json import Own_IP_Address
+from ip_add_json2 import Search_IP_Address
 
-while True:
-    #Main Menu of the Application
-    print(">>>>>>>>>>>>>> IP Address Application <<<<<<<<<<<<<<")
-    home_device = Own_IP_Address()
-    print("1 - IP Address Information")
-    print("2 - Geological Information")
-    print("3 - ISP Information")
-    print("4 - Quit")
-    choice = input("Enter your option: ")
-    print("----------------------------------------------------")
+def own_ip():
+    while True:
+        #Main Menu of the Application
+        print(">>>>>>>>>>>>>> IP Address Application <<<<<<<<<<<<<<")
+        print("-------------- Self Public IP Address --------------")
+        home_device = Own_IP_Address()
+        print("1 - IP Address Information")
+        print("2 - Geological Information")
+        print("3 - ISP Information")
+        print("4 - Quit")
+        choice = input("Enter your option: ")
+        print("----------------------------------------------------")
 
-    #Choices of the application
-    if choice == "1":
-        home_device.get_ip()    #User IP address info
-    elif choice == "2":
-        home_device.get_geological_info()   #User IP address location
-    elif choice == "3":
-        home_device.get_isp_info()      #User ISP and ASN
-    elif choice == "4":
-        #Exit the application
-        print("Thank You for using the Application!")
-        break
-    else:
-        print("Invalid Option!")
-    print("\n\n\n")
+        #Choices of the application
+        if choice == "1":
+            home_device.get_ip()    #User IP address info
+        elif choice == "2":
+            home_device.get_geological_info()   #User IP address location
+        elif choice == "3":
+            home_device.get_isp_info()      #User ISP and ASN
+        elif choice == "4":
+            #Main Menu
+            break
+        else:
+            print("Invalid Option!")
+        print("\n\n")
+
+def specific_ip():
+    while True:
+        #Main Menu of the Application
+        print(">>>>>>>>>>>>>> IP Address Application <<<<<<<<<<<<<<")
+        print("-------------- Find Public IP Address --------------")
+        ip_add = input("Enter Public IP Address: ")
+        target_device = Search_IP_Address(ip_add)
+        if ip_add == "q" or ip_add == "quit":
+            break
+        if target_device.confirmation == "Undefined" or target_device.confirmation == "None":
+            print("Invalid Public IP Address! Try Again!")
+            print("\n\n")
+        
+        else:
+            while True:
+                print("----------------------------------------------------")
+                print("1 - Geological Information")
+                print("2 - ISP Information")
+                print("3 - Quit")
+                choice = input("Enter your option: ")
+                print("----------------------------------------------------")
+
+                #Choices of the application
+                if choice == "1":
+                    target_device.get_geological_info()   #User IP address location
+                elif choice == "2":
+                    target_device.get_isp_info()      #User ISP and ASN
+                elif choice == "3":
+                    #Main Menu
+                    break
+                else:
+                    print("Invalid Option!")
+                print("\n\n")
+
+def main_menu():
+    while True:
+        #Main Menu of the Application
+        print(">>>>>>>>>>>>>> IP Address Application <<<<<<<<<<<<<<")
+        print("1 - Self Public IP Address")
+        print("2 - Find Public IP Address")
+        print("3 - Quit")
+        choice = input("Enter your option: ")
+        print("----------------------------------------------------")
+
+        #Choices of the application
+        if choice == "1":
+            own_ip()   #User Public IP address info
+        elif choice == "2":
+            specific_ip()  #Specific Public IP address info
+        elif choice == "3":
+            #Main Menu
+            print("Thank you for using the Application!")
+            break
+        else:
+            print("Invalid Option!")
+        print("\n\n")
+
+if __name__ == "__main__":
+    main_menu()
